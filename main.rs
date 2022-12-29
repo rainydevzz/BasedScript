37f9aa0db99ac7d5b319c8422efa56a155588b94
laserzz <82984448+laserzz@users.noreply.github.com>
2022-12-28 20:47:44 -0800

    LEXER WORKS RAHHHH

--- main.rs
+++ main.rs
@@ -7,15 +7,19 @@ fn main() {
 
     let mut lexer = Lexer::new(contents.unwrap());
 
-    lexer.lex();
+    let tokens = lexer.lex();
+    println!("{:?}", tokens);
 }
 
+#[derive(Debug)]
 enum TokenKind {
     String,
     Equal,
-    Identifier
+    Identifier,
+    Let
 }
 
+#[derive(Debug)]
 struct Token {
     kind: TokenKind,
     literal: String
@@ -43,26 +47,67 @@ impl Lexer {
         }
     }
 
-    pub fn lex(&mut self) {
-        let tokens: Vec<Token> = Vec::new();
+    pub fn lex(&mut self) -> Vec<Token> {
+        let mut tokens: Vec<Token> = Vec::new();
 
-        while self.counter < self.contents.len() {
-            let c = self.cur_char(); 
+        while self.contents.len() > self.counter {
+
+            if self.counter == self.contents.len() - 1 {
+                break;
+            }
             
-            match c {
-                _ if c.is_alphabetic() => {
-                    
-                },
+            match self.cur_char() {
+                _ if self.cur_char().is_alphabetic() => {
+                    let mut buf = String::new();
+                    while self.cur_char().is_alphabetic() {
+                        buf.push(self.cur_char());
+                        self.adv();
+                    }
+                    if buf == "let" {
+                        tokens.push(Token::new(TokenKind::Let, buf));
+                    } else {
+                        tokens.push(Token::new(TokenKind::Identifier, buf));
+                    }
+                }
+
+                _ if self.cur_char() == '\'' => {
+                    self.adv();
+                    let mut buf = String::new();
+                    while self.cur_char() != '\'' {
+                        buf.push(self.cur_char());
+                        self.adv();
+                    }
+                    tokens.push(Token::new(TokenKind::String, buf));
+                }
+
+                '=' => {
+                    tokens.push(Token::new(TokenKind::Equal, "=".to_owned()));
+                    self.adv();
+                }
+
+                ' ' => {
+                    self.adv();
+                }
 
                 _ => {
-                    
+                    self.adv();
                 }
             }
         }
+        return tokens;
     }
 
     fn cur_char(&self) -> char {
-        *self.contents.get(self.counter).unwrap()
+        let c = self.contents.get(self.counter);
+        
+        match c {
+            Some(value) => {
+                *c.unwrap()
+            }
+            None => {
+                '!'
+            }
+        }
     }
 
     fn adv(&mut self) {
